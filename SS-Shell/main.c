3111aa35a98c67efd28ca79c7a7c7b296e7a6859
PoeticBeam <9thadetunji@gmail.com>
2023-08-18 14:43:39 +0000

    prompt worked on

--- SS-Shell/main.c
+++ SS-Shell/main.c
@@ -5,14 +5,28 @@ int main(int ac, char **argv)
 	char *prompt = "SS-Shell >> ";
 	char *lineptr;
 	size_t n = 0;
+	ssize_t num_char_read;
 
 	/* declaring void variables */
 	(void)ac; (void)argv;
 
-	printf("%s", prompt);
-	getline(&lineptr, &n, stdin);
-	printf("%s\n", lineptr);
+	/* infinite loop */
+	while(1)
+	{
+		printf("%s", prompt);
+		num_char_read = getline(&lineptr, &n, stdin);
 
-	free(lineptr);
+		/* checks if the getline function failed or reached EOF or user CtrlD */
+		if (num_char_read == -1)
+		{
+			printf("\nLogging off <<<   \n");
+			return (-1);
+		}
+
+		printf("%s\n", lineptr);
+		
+		/* free allocated memory */
+		free(lineptr);
+	}
 	return (0);
 }
