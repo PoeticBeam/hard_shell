86263dbcfd0044d6268dd179a2610c6b0dd8d833
PoeticBeam <9thadetunji@gmail.com>
2023-08-21 11:09:25 +0000

    resolved double free detected error

--- SS-Shell/main.c
+++ SS-Shell/main.c
@@ -12,20 +12,14 @@ int main(void)
 	int x;
 	char *token;
 	char **argv;
+	/* int test_counter = 0; */
 
-	/* infinite loop */
+	/* infinite loop */ 
 	while(1)
 	{
 		printf("%s", prompt);
 		num_char_read = getline(&linepointer, &n, stdin);
 		
-		/* checks if the getline function failed or reached EOF or user CtrlD */
-		if (num_char_read == -1)
-		{
-			printf("\nLogging off <<<   \n");
-			return (-1);
-		}
-
 		/* memory allocation using return value of num_char to linepointercopy */
 		linepointercpy = malloc(sizeof(char) * num_char_read);
 		if (linepointercpy == NULL)
@@ -36,46 +30,50 @@ int main(void)
 
 		/* copies string in linepointer to linepointercopy */
 		strcpy(linepointercpy, linepointer);
-		/*now we separate by delimiter, str linepointer into array of words */
-		token = strtok(linepointer, delimiter);
 
-		/* let's see how many tokens */
-		while (token != NULL)
+		/* checks if the getline function failed or reached EOF or user CtrlD */
+		if (num_char_read == -1)
 		{
+			printf("\nLogging off <<<   \n");
+			return (-1);
+		}
+		else
+		{
+			/*now we separate by delimiter, str linepointer into array of words */
+			token = strtok(linepointer, delimiter);
+
+			/* let's see how many tokens */
+			while (token != NULL)
+			{
+				token_counter++;
+				token = strtok(NULL, delimiter);
+			}
 			token_counter++;
-			token = strtok(NULL, delimiter);
-		}
-		token_counter++;
+			/* array to store string tokens after counting characters before delimiter */		
+			argv = malloc(sizeof(char *) * token_counter);
 
-		/* array to store string tokens after counting characters before delimiter */		
-		argv = malloc(sizeof(char *) * token_counter);
+			/* we first allocated space to store the array, now we store str tokens in array */
+			for (x = 0; token != NULL; x++)
+			{
+				argv[x] = malloc(sizeof(char *) * strlen(token));
+				strcpy(argv[x], token);
 
-		/* we first allocated space to store the array, now we store str tokens in array */
-		for (x = 0; token != NULL; x++)
-		{
-			argv[x] = malloc(sizeof(char *) * strlen(token));
-			strcpy(argv[x], token);
+				token = strtok(NULL, delimiter);
+			}
+			argv[x] = NULL;
 
-			token = strtok(NULL, delimiter);
-		}
-		argv[x] = NULL;
-
-		/* test 1 */
-		/* display tokens in argv array */
-		int counter = 0;
-		for (counter = 0; counter < token_counter - 1; counter++)
-		{
-			printf("%s\n", argv[counter]);
-		}
-		/* test 1 ends */
-
-		printf("%s\n", linepointer);
+			/* test 1 */
+			/* display tokens in argv array */
+			/* for (test_counter = 0; test_counter < token_counter - 1; test_counter++)
+			{
+				printf("%s\n", argv[test_counter]);
+			} */
+			/* test 1 ends */
 		
-		/* free allocated memory */
-		free(argv);
-		free(linepointercpy);
-		free(linepointer);
-	
+			/* free allocated memory */
+			free(argv);
+			free(linepointercpy);
+		}
 	}
 	return (0);
 }
